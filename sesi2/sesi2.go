a775881965a4315b341f07bb3be416efb327b93b
Arif Laksonodhewo <arifldhewo234@gmail.com>
2024-05-22 01:43:34 +0700

    TPM 2

---
+++ sesi2/sesi2.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+	var panjang int = 5
+	var initiate int = 1
+	result := []int{}
+
+	for i := 0; i < panjang; i++ {
+		if len(result) == 0 {
+			result = append(result, initiate)
+		} else {
+			initiate += 2
+			result = append(result, initiate)
+		}
+	}
+
+	lastVal := result[len(result)-1]
+
+	for i := 0; i < panjang; i++ {
+
+		var startIndex int
+		var lastIndex int
+
+		if lastVal == result[i] {
+			startIndex = 0
+			lastIndex = lastVal - 1
+		} else {
+			startIndex = (lastVal - result[i]) / 2
+			if result[i] == 1 {
+				lastIndex = startIndex
+			} else {
+				lastIndex = (startIndex + result[i]) - 1
+			}
+		}
+
+		for counter := 0 ; ; {
+
+			if(counter >= startIndex && counter <= lastIndex)  {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+
+			if(counter == lastVal) {
+				fmt.Println()
+				break
+			}else {
+				counter++
+			}
+
+		}
+	}
+}
